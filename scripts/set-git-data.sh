5f4c006cc87581532c21ebe6862a33785da7630c
yasabesquien <1903837+yasabesquien@users.noreply.github.com>
2021-07-01 22:11:04 -0300

    acomodando el set-git-data

--- scripts/set-git-data.sh
+++ scripts/set-git-data.sh
@@ -1,13 +1,34 @@
-echo
-echo " para todos los repos... "
-echo " git config --global user.email "you@example.com""
-echo " git config --global user.name "Your Name""
-echo
 
-echo 
-echo " solo para el repo local... "
-echo " git config user.email "1903837+yasabesquien@users.noreply.github.com""
-echo " git config user.name "yasabesquien""
-git config user.email "1903837+yasabesquien@users.noreply.github.com"
-git config user.name "yasabesquien"
-echo
+if [ $1 ];then
+
+  cd /pruebas/tools/scripts/
+  echo
+  echo " para todos los repos... "
+  echo " git config --global user.email "you@example.com""
+  echo " git config --global user.name "Your Name""
+  echo
+
+  echo 
+  echo " solo para el repo local... "
+  echo " git config user.email "1903837+yasabesquien@users.noreply.github.com""
+  echo " git config user.name "yasabesquien""
+  git config user.email "1903837+yasabesquien@users.noreply.github.com"
+  git config user.name "yasabesquien"
+  echo
+  echo " asignando la key para firmar los commits..."
+  ./configurar-key-gpg-para-firmar-commits-en-github.sh 07177A1AB72B7F4A
+  echo " activando la firma de commits de forma global..."
+  ./activar-firmado-gpg-github.sh y
+
+  echo " cargando la key de ssh para hacer los push..."
+  echo " ssh-add $1 "
+
+else
+
+  echo
+  echo " Carga automaticamente en memoria las claves gpg, ssh y activa la firma de los commits... "
+  echo
+  echo " uso:$0 <ssh-key> "
+  echo
+
+fi
