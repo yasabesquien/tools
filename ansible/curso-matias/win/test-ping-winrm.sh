903609d1de09484f859b5b9fddfdc830623b97b3
yasabesquien <1903837+yasabesquien@users.noreply.github.com>
2021-07-08 00:12:09 -0300

    agregando recetas de ansible para windows

--- ansible/curso-matias/win/test-ping-winrm.sh
+++ ansible/curso-matias/win/test-ping-winrm.sh
@@ -1,1 +1,6 @@
-ansible -i inventory/demo.yaml -m win_ping all
+
+#ansible -i inventory/demo.yaml -m win_ping all
+
+ansible -i inventory/conf-winrc-base.yaml -m win_ping all
+
+#ansible -i inventory/demo.yaml -m win_ping all
